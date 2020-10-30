803d5a65edc852fcb30a099d075e280f42459a38
unknown <grisha1996111616@gmail.com>
2020-10-30 19:51:45 +0000

    just

--- src/java-script/parts/initialHomePages.js
+++ src/java-script/parts/initialHomePages.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// - создаем глобальные переменные renderFilms и genres, pageNumber (будет использоваться
+ в запросе при плагинации);
+// - создаем функцию createCardFunc, она принимает параметрами imgPath, filmTitle, movieId 
+создает li согласно макета, вешает на нее слушателем функцию activeDetailsPage
+ c параметрами movieId и флагом false так как фильм из библиотеки (смотри пункт “3)” создание
+  activeDetailsPage);
+// - создаем функцию fetchPopularMoviesList (должна в запросе в виде переменной использовать pageNumber) в которой используется createCardFunc результат используя fragment кладем в ul, и не забываем заполнить этими же данными переменную renderFilms (она понадобится в работе следующим участникам);
+// - создаем функцию fetchGenres которая забирает жанры и кладет их в переменную genres (она понадобится в работе следующим участникам);
+// - запускаем функцию fetchPopularMoviesList и fetchGenres.
 import homepageGalleryTpl from '../templates/homepage-gallery.hbs';
 
 const refs = {
