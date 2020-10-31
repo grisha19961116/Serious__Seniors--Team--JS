39c3ca58e543889df18c9f1bec5d91e77bbb9709
unknown <grisha1996111616@gmail.com>
2020-10-31 07:42:45 +0000

    worked with navigation and cleaned litle bit index html delete not necesrey btn next and prev

--- src/java-script/parts/navigation.js
+++ src/java-script/parts/navigation.js
@@ -6,13 +6,36 @@ console.log(refsNavigation.homePageSection);
 console.log(refsNavigation.filmLibraryPageSection);
 console.log(refsNavigation.buttonShowLIstQueue);
 console.log(refsNavigation.buttonFilmsWatched);
+console.log(refsNavigation.buttonAddFilmToQueue);
+console.log(refsNavigation.buttonAddFilmToWatched);
 console.log(refsNavigation.buttonPrev);
 console.log(refsNavigation.buttonNext);
-// const buttonAddFilm = document.querySelector('кнопка добавления фильмов в очередь просмотра');
-// const buttonDeleteFilm = document.querySelector('кнопка удаления фильмов з очереди просмотра');
-// const buttonAddFilmQueueToWatched = document.querySelector('кнопка добавления фильмов  из просмотренных со страницы detailsPage');
-// const buttonDeleteFilmQueueFromWatched = document.querySelector('кнопка удаления фильмов  из просмотренных со страницы detailsPage');
-let selectFilm = {some:`dataConnected with local storage`};
+// const buttonDeleteFilmFromQueue = document.querySelector('кнопка удаления фильмов з очереди просмотра');
+// const buttonDeleteFilmFromWatched = document.querySelector('кнопка удаления фильмов  из просмотренных со страницы detailsPage');
+let selectFilm = {some:`dataConnected with local storage`};   
+// добавления/удаления фильмов в очередь просмотра и добавления/удаления фильмов из просмотренных
+//  со страницы detailsPage; 
+// - создаем глобальную переменную selectFilm; 
+// - в main.scss создаем класс чтобы прятать секции (потом его добавляем или удаляем нужной секции);
+//  - создаем функцию activeHomePage которая показывает домашнюю страницу и прячет остальные,
+//   а также вешает слушателей на кнопку вперед и назад из плагинации и удаляет ненужных всех 
+//   слушателей (таких 4 во всем проекте не нужных на этой странице); 
+// - создаем функцию activeLibraryPage которая показывает страницу с библиотекой и прячет остальные,
+//  запускает функцию отрисовки фильмов из очереди drawQueueFilmList (которую сделает пятый участник) 
+//  и добавляет кнопке списка очереди фильмов эффект выбранной с помощью класса, а также вешает слушателей 
+//  на кнопки показа очереди фильмов и просмотренных фильмов и удаляет ненужных всех слушателей 
+//  (таких 4 во всем проекте не нужных на этой странице); 
+// - создаем функцию activeDetailsPage которая показывает страницу детальной отрисовки фильма и
+//  прячет остальные, функция принимает два параметра movieId и itsLibraryFilm (это bool), 
+//  и в зависимости от того это выбранный фильм с домашней страницы или из библиотеки, 
+//  заполняет глобальную переменную selectFilm нужным объектом и запускает функцию  showDetails
+//   c параметром selectFilm который они заполнили одними или другими данными (которую сделает 4й участник),
+//    вешает слушателей на кнопки добавления/удаления фильмов в очередь просмотра и добавления/удаления фильмов 
+//    из просмотренных со страницы detailsPage
+//     и удаляет ненужных всех слушателей (таких 4 во всем проекте не нужных на этой странице); 
+// - вешаем слушателей на переход на домашнюю страницу и страницу библиотеки в хедере.
+// - на логотип повесить запуск функции activeHomePage, чтобы при клике туда возвращаться.
+
 const activeHomePage = () =>{
     console.log(`hello from navigation.js of activeHomePage function`);
 
